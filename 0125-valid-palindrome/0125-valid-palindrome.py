2d23af5b37a2f19ca6c3b59b041a4d245876ebf2
Praharsh R Dubey <75966397+praharsh05@users.noreply.github.com>
2025-01-14 22:13:45 +0000

    Time: 15 ms (14.4%), Space: 18.2 MB (41.52%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -3,9 +3,11 @@ class Solution:
         temp_string = ''
         x=0
         for y in range(len(s)):
+            # Check for lower case and 0-9
             if (ord(s[y]) >= 97 and ord(s[y]) <= 122) \
                 or (ord(s[y]) >= 48 and ord(s[y]) <= 57):
                 temp_string += s[y]
+            # Check for upper case and conver to lower
             elif ord(s[y]) >= 65 and ord(s[y]) <= 90:
                 temp_string += s[y].lower()
         if temp_string == temp_string[::-1]: return True
