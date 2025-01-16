3b5aa7e9086c6b7867ab4f0937577216a79db583
Praharsh R Dubey <75966397+praharsh05@users.noreply.github.com>
2025-01-16 14:54:46 +0000

    Time: 0 ms (100%), Space: 17.8 MB (37.77%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,37 +1,7 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         
-        # # Check Rows
-        # for i in range(9):
-        #     valid_set = set()
-        #     for j in range(9):
-        #         if board[i][j] != ".":
-        #             if board[i][j] in valid_set:
-        #                 return False
-        #             valid_set.add(board[i][j])
-                        
-        # # Check Columns
-        # for j in range(9):
-        #     valid_set = set()
-        #     for i in range(9):
-        #         if board[i][j] != ".":
-        #             if board[i][j] in valid_set:
-        #                 return False
-        #             valid_set.add(board[i][j]) 
-
-        # # Check sub-box
-        # for x in range(0,9,3):
-        #     for y in range(0,9,3):
-        #         valid_set = set()
-        #         for i in range(x, x+3):
-        #             for j in range(y, y+3):
-        #                 if board[i][j] != ".":
-        #                     if board[i][j] in valid_set:
-        #                         return False
-        #                     valid_set.add(board[i][j])
-                                
-        # return True
-
+        # Create dict for rows, columns and boxes
         rows = defaultdict(set)
         cols = defaultdict(set)
         boxes = defaultdict(set)
@@ -39,7 +9,6 @@ class Solution:
         for r in range(9):
             for c in range(9):
                 if board[r][c] != ".":
-                    
                     if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in boxes[(r // 3, c // 3)]:
                         return False
                     
