12c565bcc8757c3175a6e0752c36dd9e3808c218
Praharsh R Dubey <75966397+praharsh05@users.noreply.github.com>
2025-01-30 09:18:15 +0000

    Time: 0 ms (100%), Space: 21.9 MB (37.87%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,44 +3,6 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        
-        # def dfs(row, col, curr, board):
-        #     # mark the current cell as visited
-        #     curr[row][col] = True
-        #     n, m = len(board), len(board[0])
-
-        #     # Directions for traversing
-        #     directions = [[1,0], [-1,0], [0,1], [0,-1]]
-
-        #     for dr, dc in directions:
-        #         nrow, ncol = row + dr, col + dc
-        #         # Check bounds, visitation, and if its a O
-        #         if 0 <= nrow < n and 0 <= ncol < m and not curr[nrow][ncol] and board[nrow][ncol] == "O":
-        #             dfs(nrow, ncol, curr, board)
-        
-        # n, m = len(board), len(board[0])
-        # # Visited matrix to track process cells
-        # vis = [[False] * m for _ in range(n)]
-
-        # # Traverse border rows
-        # for i in range(n):
-        #     if not vis[i][0] and board[i][0] == "O":
-        #         dfs(i, 0, vis, board)
-        #     if not vis[i][m-1] and board[i][m-1] == "O":
-        #         dfs(i, m-1, vis, board)
-        
-        # # Traverse border cols
-        # for j in range(m):
-        #     if not vis[0][j] and board[0][j] == "O":
-        #         dfs(0, j, vis, board)
-        #     if not vis[n-1][j] and board[n-1][j] == "O":
-        #         dfs(n-1, j, vis, board)
-
-        # # Flip unvisted "O" to "X", retain visited "O" as it is
-        # for i in range(n):
-        #     for j in range(m):
-        #         if not vis[i][j] and board[i][j] == "O":
-        #             board[i][j] = "X"
 
         def dfs(row, col):
             """Mark 'O' connected to borders with '#'"""
