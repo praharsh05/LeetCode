4117216a5b57596c51f93e3d12359184705a4125
Praharsh R Dubey <75966397+praharsh05@users.noreply.github.com>
2025-02-01 20:46:10 +0000

    Time: 0 ms (100%), Space: 17.9 MB (59.85%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,17 +1,15 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         
-        dic = defaultdict(list)
-        for i in range(len(equations)):
-            dic[equations[i][0]].append((equations[i][1], values[i]))
-            dic[equations[i][1]].append((equations[i][0], 1/values[i]))
         
-        # DFS to search the relation to a query
+        # DFS to search the relation to a query in the graph
         def dfs(graph, start, target):
-
+            
+            # If start or target not in the graph then return -1 as there is no relationship
             if start not in graph or target not in graph:
                 return -1
             
+            # Create a stack and add the start (A[i]) and set the ans as 1 as A[i]/A[i] = 1
             stack = []
             stack.append((start, 1))
             visited = set()
@@ -25,10 +23,16 @@ class Solution:
                 
                 for neighbor, div in graph[node]:
                     if neighbor not in visited:
+                        # Example: A/C = A/B * B/C
                         stack.append((neighbor, ans*div))
             
             return -1
         
+        # Create a Graph
+        dic = defaultdict(list)
+        for i in range(len(equations)):
+            dic[equations[i][0]].append((equations[i][1], values[i]))
+            dic[equations[i][1]].append((equations[i][0], 1/values[i]))
         ans = []
 
         for x, y in queries:
