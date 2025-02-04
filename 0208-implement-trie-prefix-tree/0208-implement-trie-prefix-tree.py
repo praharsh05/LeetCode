18fcfc2b36ee31f8d0de6fc923cfc88d7fa00ff4
Praharsh R Dubey <75966397+praharsh05@users.noreply.github.com>
2025-02-04 20:50:21 +0000

    Time: 75 ms (16.23%), Space: 35.3 MB (9.88%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,40 +1,43 @@
 class TrieNode:
     def __init__(self):
-        self.children = {}
+        self.children = [None] * 26 # Fixed array for a-z
         self.is_end = False
 
 class Trie:
 
     def __init__(self):
-        self.trie = TrieNode()
+        self.root = TrieNode()
 
     def insert(self, word: str) -> None:
-        curr = self.trie
+        curr = self.root
         for char in word:
-            # If a char of word does not exist in the children of the node then add it to the children
-            if char not in curr.children:
-                curr.children[char] = TrieNode()
-            # Move the current node to the children node
-            curr = curr.children[char]
+            # If the index of the char does not exist in the children of the node then add it to the children
+            index = ord(char) - ord('a')
+            if not curr.children[index]:
+                curr.children[index] = TrieNode()
+            # Move the current node to the child node
+            curr = curr.children[index]
         curr.is_end = True
 
     def search(self, word: str) -> bool:
         # Start from the trie root
-        curr = self.trie
+        curr = self.root
         for char in word:
-            if char not in curr.children: return False
+            index = ord(char) - ord('a')
+            if not curr.children[index]: return False
 
             # Move to child node
-            curr = curr.children[char]
+            curr = curr.children[index]
         
         return curr.is_end
 
     def startsWith(self, prefix: str) -> bool:
-        curr = self.trie
+        curr = self.root
         for char in prefix:
-            if char not in curr.children: return False
+            index = ord(char) - ord('a')
+            if not curr.children[index]: return False
 
-            curr = curr.children[char]
+            curr = curr.children[index]
         
         return True
 
