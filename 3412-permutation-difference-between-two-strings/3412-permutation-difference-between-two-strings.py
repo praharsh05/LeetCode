ac17db7485b4b55a933fa85945c9180587da13f1
Praharsh R Dubey <75966397+praharsh05@users.noreply.github.com>
2025-02-17 01:00:46 +0000

    Time: 0 ms (100%), Space: 17.8 MB (48.91%) - LeetHub

---
+++ 3412-permutation-difference-between-two-strings/3412-permutation-difference-between-two-strings.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findPermutationDifference(self, s: str, t: str) -> int:
+        total = 0
+        for i in range(len(s)):
+            total += abs(i - t.index(s[i]))
+        
+        return total
