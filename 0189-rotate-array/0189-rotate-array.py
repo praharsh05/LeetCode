ece71d20006d1e10698bd69b7b50b731de2b02f9
Praharsh R Dubey <75966397+praharsh05@users.noreply.github.com>
2025-04-29 21:43:59 +0100

    Time: 1 ms (77.68%), Space: 25.9 MB (5.08%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -5,5 +5,4 @@ class Solution:
         """
         k = k % len(nums)
         if k != 0:
-            nums[:k], nums[k:] = nums[-k:], nums[:-k]
-        
+            nums[k:], nums[:k] = nums[:-k], nums[-k:]
