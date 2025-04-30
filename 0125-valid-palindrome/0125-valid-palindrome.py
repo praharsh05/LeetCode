01477003f4b50a3f92687c287d828dccb8241a03
Praharsh R Dubey <75966397+praharsh05@users.noreply.github.com>
2025-04-30 19:20:29 +0100

    Time: 7 ms (80.9%), Space: 18.1 MB (69.97%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,14 +1,9 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        temp_string = ''
-        x=0
-        for y in range(len(s)):
-            # Check for lower case and 0-9
-            if (ord(s[y]) >= 97 and ord(s[y]) <= 122) \
-                or (ord(s[y]) >= 48 and ord(s[y]) <= 57):
-                temp_string += s[y]
-            # Check for upper case and conver to lower
-            elif ord(s[y]) >= 65 and ord(s[y]) <= 90:
-                temp_string += s[y].lower()
-        if temp_string == temp_string[::-1]: return True
-        return False
+        temp_string = ""
+
+        for i in range(len(s)):
+            if s[i].isalnum():
+                temp_string += s[i].lower()
+        
+        return temp_string == temp_string[::-1]
